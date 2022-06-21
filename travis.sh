651e61deffe184df4c5eff2767b22e826b87330d
Georg Richter <georg@mariadb.com>
2022-06-21 08:04:14 +0200

    travis fix

--- travis.sh
+++ travis.sh
@@ -61,7 +61,6 @@ else
   mkdir bld
   cd bld
   if [ "$TRAVIS_OS_NAME" = "osx" ] ; then
-    brew install mariadb && mysql.server start;
     cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCERT_PATH=${SSLCERT} -DWITH_SSL=GNUTLS
   else
     cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCERT_PATH=${SSLCERT}
